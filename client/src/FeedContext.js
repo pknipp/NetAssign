7bcb77506291c620272deb9950104bd33abb57f1
Peter Knipp <peteraknipp@gmail.com>
2021-01-01 07:44:21 -0500

    Replaced AllPosts component with a Success component

--- client/src/FeedContext.js
+++ client/src/FeedContext.js
@@ -1,5 +1,3 @@
 import React from 'react';
-
 const FeedContext = React.createContext();
-
 export default FeedContext;
--- client/src/PostContext.js
+++ client/src/PostContext.js
@@ -1,6 +1,3 @@
 import React, { useEffect, useContext } from 'react';
-
-
 const PostContext = React.createContext();
-
 export default PostContext;
