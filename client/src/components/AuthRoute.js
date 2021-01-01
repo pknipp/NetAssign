fed1b2db41bdb035c03457c079fb8326d7dff588
Peter Knipp <peteraknipp@gmail.com>
2021-01-01 10:49:15 -0500

    Unstyle and tighten up SignUp component

--- client/src/components/AuthRoute.js
+++ client/src/components/AuthRoute.js
@@ -3,7 +3,7 @@ import { Redirect, Route } from 'react-router-dom';
 import AuthContext from "../auth"
 
 const ProtectedRoute = ({ component: Component, path, exact }) => {
-    const { currentUserId } = useContext(AuthContext)
+    const { currentUserId } = useContext(AuthContext);
     return (
         <Route path={path} exact={exact} render={props => currentUserId ? <Redirect to="/" /> : <Component />} />
     )
