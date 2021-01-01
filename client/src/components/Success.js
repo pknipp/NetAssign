13d74db21e5f9ef2c230d000dcfb8ee846f534ba
Peter Knipp <peteraknipp@gmail.com>
2021-01-01 07:50:03 -0500

    Clean up file names and locations before proceeding.

---
+++ client/src/components/Success.js
@@ -0,0 +1,3 @@
+import React from 'react'
+const Success = _ =>  <h1>Logged In!</h1>
+export default Success
--- client/src/components/feed/AllPosts.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-function AllPosts() {
-    return <h1>Logged In!</h1>
-}
-export default AllPosts
