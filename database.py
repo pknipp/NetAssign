43191b1227347213de8cbc5513405b7d7996836c
Peter Knipp <peteraknipp@gmail.com>
2021-01-03 12:43:55 -0500

    Seeded models now include Users, Questions, Courses, and Enrollments

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from mywebassign.models import User, Question, Course
+from mywebassign.models import User, Question, Course, Enrollment
 from mywebassign import app, db
 from dotenv import load_dotenv
 from datetime import date, datetime, timedelta
@@ -17,17 +17,17 @@ users = [
     ]
 
 # number of non-demo users (including demo users)
-n_teacher = 5
-n_student = 25
+n_teachers = 8
+n_students = 40
 
 with app.app_context():
     db.drop_all()
     db.create_all()
-    for i in range(n_teacher + n_student):
+    for i in range(n_teachers + n_students):
         created_at = fake.date_time_between(start_date=datetime(2000, 1, 15))
         db.session.add(User(
             email= users[i][0] if (i < 2) else fake.simple_profile()["mail"],
-            isTeacher= i == 0 or (i > 1 and i < n_teacher + 1),
+            isTeacher= i == 0 or (i > 1 and i < n_teachers + 1),
             password="password",
             created_at=created_at,
             updated_at=fake.date_time_between(start_date=created_at)
@@ -96,14 +96,29 @@ with app.app_context():
 
     for i in range(len(courses)):
         created_at = fake.date_time_between(start_date=datetime(2000, 1, 15))
-        if (i == 1 or random() < 1/n_teacher):
-            teacher_id = 1
-        else:
-            teacher_id = randrange(2, 2 + n_teacher)
+        teacher_id = 1 if (i == 0 or random() < 1 / n_teachers) else randrange(3, 2 + n_teachers)
+        courses[i] = [teacher_id, courses[i]]
         db.session.add(Course(
             teacher_id=teacher_id,
-            name=courses[i],
+            name=courses[i][1],
             created_at=created_at,
             updated_at=created_at,
         ))
+    db.session.commit()
+
+# average number of enrollments per student
+enrollments_per_student = 4
+
+with app.app_context():
+    for i in range(len(courses)):
+        course_id = i + 1
+        teacher_id = courses[i][0]
+        for j in range(n_teachers + n_students):
+            student_id = j + 1
+            if (student_id == 2 and course_id == 1) or (student_id == teacher_id) or (random() < enrollments_per_student/len(courses)):
+                db.session.add(Enrollment(
+                    course_id=course_id,
+                    student_id=student_id,
+                    created_at=fake.date_time_between(start_date=datetime(2000, 1, 15))
+                ))
     db.session.commit()
